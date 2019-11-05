ee658924cf0c8317214eb99c9d9c2d0f8806739f
rjwehrle <rjw@ryans-macbook.lan>
2019-11-04 22:08:32 -0500

    Implemented functions to calculate the future value or present value of a single cash flow compounded discretely or continuously

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * Calculates the future value of a single cash flow
+ * @param pv - present value of the cash flow
+ * @param r - annual interest rate
+ * @param t - number of years
+ * @param m - number of compounding periods per year (if null or undefined then
+ * cash flow is calculated using continuous compounding)
+ */
+exports.fv = function(pv, r, t, m) {
+    // Continuous compounding
+    if (m === null || m === undefined) {
+        return pv * Math.exp(r * t);
+    }
+    // Discrete compounding
+    else {
+        return pv * Math.pow(1 + r / m, m * t);
+    }
+}
+
+/**
+ * Calculates the present value of a single cash flow
+ * @param pv - future value of the cash flow
+ * @param r - annual interest rate
+ * @param t - number of years
+ * @param m - number of compounding periods per year (if null or undefined then
+ * cash flow is calculated using continuous compounding)
+ */
+exports.pv = function(fv, r, t, m) {
+    // Continuous compounding
+    if (m === null || m === undefined) {
+        return fv / Math.exp(r * t);
+    }
+    // Discrete compounding
+    else {
+        return fv / Math.pow(1 + r / m, m * t);
+    }
+}
